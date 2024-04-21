899b5eb00a46d88cb5c82b6994c131b09f63be01
rjpjuffermans <110410711+rjpjuffermans@users.noreply.github.com>
2024-04-22 00:06:26 +0200

    Delete scripts/mySudoku.js

--- scripts/mySudoku.js
+++
@@ -1,9 +0,0 @@
-import { sudoku } from "./sudoku-class.js"
-import { timer } from "./timer.js";
-import { renderDifficulty } from "./difficulty.js"
-import { renderHighscores } from "./highscores.js";
-import { renderGameControls } from "./game-controls.js";
-
-renderDifficulty();
-renderHighscores();
-renderGameControls(sudoku, timer);
